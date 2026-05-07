978fbc52172995aa1576d4bd8a0af76cb0c6001f
Roshni Ratnani <roshniratnani2410@gmail.com>
2026-05-07 19:03:54 +0530

    refs: initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT ?? 8080;
+
+app.get('/', (req,res)=>{
+  return res.json({msg:`Hello Everyone`});
+})
+
+app.listen(port,()=>{
+  console.log("running on port", port);
+});
